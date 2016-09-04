5a99928d9c24b7f800654371515fee7cdcfcb654
Apoorva Pandey <apoorvaeternity@gmail.com>
2016-09-04 22:46:54 +0530

    signup model

--- onlineAuction/signup/models.py
+++ onlineAuction/signup/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class UserDetail(models.Model):
+	def __str__(self):
+		return self.email
+	userID=models.CharField(max_length=30)
+	name = models.CharField(max_length=30)
+	password=models.CharField(max_length=30)
+	email=models.CharField(max_length=50)
+    bidCoins= models.IntegerField(default=0)
+
+
+
+
+
+
