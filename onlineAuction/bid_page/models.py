5f6d86805a7f92cb16fcc26df10b5de2d0345bc1
Apoorva Pandey <apoorvaeternity@gmail.com>
2016-09-07 02:14:50 +0530

    bid_page app bids class added in models.py

--- onlineAuction/bid_page/models.py
+++ onlineAuction/bid_page/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from signup import models as userdetails
+from articles import models as articledetails
 # Create your models here.
+
+class bids(models.Model):
+    userid=models.ForeignKey(userdetails.UserDetail)
+    articleid=models.ForeignKey(articledetails.articlereg)
+    highestbid=models.FloatField(default=0)
