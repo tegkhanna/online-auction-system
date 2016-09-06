ff7a8e46c6663211a01027280410ab070b9a78fb
Apoorva Pandey <apoorvaeternity@gmail.com>
2016-09-07 01:54:51 +0530

    articles app models created

--- onlineAuction/articles/models.py
+++ onlineAuction/articles/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from signup import models as userdetails
 # Create your models here.
+class articlereg(models.Model):
+    userid=models.ForeignKey(userdetails.UserDetail)
+    status=models.CharField(max_length=10)
+    timestart=models.DateTimeField()
+    articlename=models.CharField(max_length=40)
+    category=models.CharField(max_length=40)
+    desc=models.CharField(max_length=150)
+    minbid=models.IntegerField(default=0)
+    image=models.ImageField(upload_to='article_images/',default='article_images/'+str(self.id)+'.jpg')
+
+
+
+
--- onlineAuction/signup/models.py
+++ onlineAuction/signup/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class UserDetail(models.Model):
 	def __str__(self):
-		return self.email
+		return self.id
 	userName=models.CharField(max_length=30)
 	name = models.CharField(max_length=30)
 	password=models.CharField(max_length=30)
