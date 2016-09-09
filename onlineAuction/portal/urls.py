d566586f44e7d2e9dbe5fd219bf594a1d5f983a6
teg khanna <teg khanna>
2016-09-09 20:00:19 +0530

    error again :( sorry this one is last ..actually merge created 2 different implementation of the same thing

--- onlineAuction/portal/urls.py
+++ onlineAuction/portal/urls.py
@@ -6,7 +6,6 @@ app_name = 'portal'
 
 urlpatterns = [
 
-	url(r'^$', views.index, name='index'),
 	url(r'^adminPage/$', views.adminPage, name='adminPage'),
 	url(r'delete/(?P<id>\d+)/$', views.deleteUser, name='delete'),
 	url(r'ban/(?P<id>\d+)/$', views.banUser, name='ban'),
