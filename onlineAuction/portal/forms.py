e4ef4ac518e6f820a9ccaef502b1d8870e48105a
Apoorva Pandey <apoorvaeternity@gmail.com>
2016-09-12 02:51:11 +0530

    Dropdown added for categories in Article Registration Form using ChoiceField

--- onlineAuction/portal/forms.py
+++ onlineAuction/portal/forms.py
@@ -5,7 +5,9 @@ class RegForm(forms.Form):#user form pre build class
     status=forms.CharField(max_length=10)
     timestart=forms.DateTimeField()
     articlename=forms.CharField(max_length=40)
-    category=forms.CharField(max_length=40)
+    Choices=(('Book','Book'),('Vehicle','Vehicle'),('Coin or Stamp','Coin or Stamp'),('Antique','Antique'),
+             ('Electronics','Electronics'),('Others','Others'))
+    category=forms.ChoiceField(choices=Choices)
     desc=forms.CharField(max_length=150)
     minbid=forms.FloatField()
     image=forms.ImageField()
