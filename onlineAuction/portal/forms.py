656aed8f6778c726cc96915e1d04802a5e4776d1
TEG KHANNA <tegkhanna@gmail.com>
2016-09-14 17:20:52 +0530

    auto status,date time error removed

--- onlineAuction/portal/forms.py
+++ onlineAuction/portal/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import *
 
 class RegForm(forms.Form):#user form pre build class
+    
     timestart=forms.DateTimeField()
     articlename=forms.CharField(max_length=40)
     Choices=(('Book','Book'),('Vehicle','Vehicle'),('Coin or Stamp','Coin or Stamp'),('Antique','Antique'),
--- onlineAuction/portal/views.py
+++ onlineAuction/portal/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect, Http404, HttpResponse
 from django.urls import reverse
 from django.views import generic
-from django.utils import timezone
+from django.utils import timezone, dateparse
 # Create your views here.
 from datetime import datetime, timedelta
 from signup.models import UserDetail
@@ -92,19 +92,19 @@ class RegForm(generic.edit.FormView):
     def post(self, request, *args, **kwargs):
         try:
             quer = UserDetail.objects.get(pk=request.session['userID'])
-           # time = clean(request.POST['timestart'])
-           # now = datetime.now()
-           # deltaNow = timedelta(days = int(now.day),hours = int(now.hour),minutes = int(now.minute),seconds = int(now.second))
-           # delta = timedelta(days = int(time.day),hours = int(time.hour),minutes = int(time.minute),seconds = int(time.second))
-          #  timedif = (deltaNow - delta).seconds
-          #  stat = None
-           # if(time < datetime.datetime.now()):
-           #     stat = "sold"
-           # elif(timedif > 3600):
-            #    stat = "inactive"
-            #elif(timedif < 3600 and timedif > 0):
-            #    stat = "active"
-            art=quer.articlereg_set.create( status = "active",
+            time = dateparse.parse_datetime(request.POST['timestart'])
+            now = datetime.now()
+            deltaNow = timedelta(days = int(now.day),hours = int(now.hour),minutes = int(now.minute),seconds = int(now.second))
+            delta = timedelta(days = int(time.day),hours = int(time.hour),minutes = int(time.minute),seconds = int(time.second))
+            timedif = (deltaNow - delta).seconds
+            stat = None
+            if(time < datetime.now()):
+                return HttpResponse("ENTER CORRECT DATE TIME")
+            elif(timedif > 3600):
+                stat = "inactive"
+            elif(timedif < 3600 and timedif > 0):
+                stat = "active"
+            art=quer.articlereg_set.create( status = stat,
                                         timestart=request.POST['timestart'],articlename=request.POST['articlename'],
                                         category=request.POST['category'],desc=request.POST['desc'],
                                         minbid=request.POST['minbid'])
@@ -118,8 +118,7 @@ class EditArticle(generic.edit.FormView):
     template_name = 'portal/articleForm.html'
     def get(self, request,a_id, *args, **kwargs):
         data = articlereg.objects.get(pk=int(a_id))
-        form = self.form_class(initial = {'status' : data.status, 
-                                        'timestart':data.timestart,
+        form = self.form_class(initial = {'timestart':data.timestart,
                                         'articlename':data.articlename,
                                         'category':data.category,
                                         'desc':data.desc,
@@ -133,14 +132,29 @@ class EditArticle(generic.edit.FormView):
         try:
             quer = UserDetail.objects.get(pk=request.session['userID'])
             art = quer.articlereg_set.get(pk=int(a_id))
-            art.status = request.POST['status']
             art.timestart=request.POST['timestart']
             art.articlename=request.POST['articlename']
             art.category=request.POST['category']
-            art.desc=request.POST['desc'],
+            art.desc=request.POST['desc']
             art.minbid=request.POST['minbid']
             img = art.articleimage_set.reverse()[0]
             img.image =request.FILES['image']
+            #status update
+            time = dateparse.parse_datetime(request.POST['timestart'])
+            now = datetime.now()
+            deltaNow = timedelta(days = int(now.day),hours = int(now.hour),minutes = int(now.minute),seconds = int(now.second))
+            delta = timedelta(days = int(time.day),hours = int(time.hour),minutes = int(time.minute),seconds = int(time.second))
+            timedif = (deltaNow - delta).seconds
+            stat = None
+            if(timedif < 3600 and timedif > 0):
+                stat = "active"
+            elif(time < datetime.now()):
+                return HttpResponse("ENTER CORRECT DATE TIME")
+            elif(timedif > 3600):
+                stat = "inactive"
+
+            art.status = stat
+
             img.save()
             art.save()
             return HttpResponseRedirect(reverse("portal:userArticles"))
